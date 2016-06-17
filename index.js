13f3d1005cb2150715a1451aeb8ff70824c7034d
KadoBOT <ricardo.ambrogi@gmail.com>
2016-06-17 07:31:06 -0300

    Created React Toasted

---
+++ index.js
@@ -0,0 +1,29 @@
+import {render} from 'react-dom'
+import './toasted.scss'
+
+const Toasted = (children, className = 'info', timeout = 5000) => {
+  let duration = timeout/1000
+  let elementExists = document.getElementById('toasted')
+
+  let ul = document.createElement('ul')
+  ul.id = 'toasted'
+
+  let node = document.createElement('li')
+  node.className = `toasted ${className}`
+  node.style.animation = `showToaster ${duration}s`
+
+  if(elementExists) {
+    elementExists.appendChild(node)
+    render(children, node)
+  } else {
+    document.body.appendChild(ul)
+    ul.appendChild(node)
+    render(children, node)
+  }
+
+  setTimeout(() => {
+    document.getElementById('toasted').removeChild(node)
+  }, timeout - 50)
+}
+
+export default Toasty
