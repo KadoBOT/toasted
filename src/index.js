fd68cee9527b37e922e4e96acd692100d3afd0c5
KadoBOT <ricardo.ambrogi@gmail.com>
2016-06-19 10:14:18 -0300

    added inline css

--- src/index.js
+++ src/index.js
@@ -1,29 +1,156 @@
-const Toasted = (children, className = 'info', timeout = 5000) => {
+const style = document.createElement('style')
 
-    document.write('<link rel="stylesheet" type="text/css" href="./node_modules/toasted/toasted.css">')
+//You can customize the toast CSS here
+const css = `@keyframes bottom {
+  0% {
+    opacity: 0;
+    transform: translateY(0px);
+  }
+  20% {
+    opacity: 1;
+    transform: translateY(-25px);
+  }
+  80% {
+    opacity: 1;
+    transform: translateY(-25px);
+  }
+  100% {
+    opacity: 0;
+    transform: translateY(0px);
+  }
+}
 
-    let duration = timeout/1000
-    let elementExists = document.getElementById('toasted')
+@keyframes top {
+  0% {
+    opacity: 0;
+    transform: translateY(0px);
+  }
+  20% {
+    opacity: 1;
+    transform: translateY(25px);
+  }
+  80% {
+    opacity: 1;
+    transform: translateY(25px);
+  }
+  100% {
+    opacity: 0;
+    transform: translateY(0px);
+  }
+}
 
-    let ul = document.createElement('ul')
-    ul.id = 'toasted'
+.onTop--true{
+  transform: rotate(180deg);
+}
 
-    let node = document.createElement('li')
-    node.className = `toasted ${className}`
-    node.style.animation = `showToaster ${duration}s`
+.onTop--true li{
+  transform: rotate(-180deg);
+}
 
-    if(elementExists) {
-        elementExists.appendChild(node)
-        ReactDOM.render(children, node)
-    } else {
-        document.body.appendChild(ul)
-        ul.appendChild(node)
-        ReactDOM.render(children, node)
-    }
+.bottom {
+  bottom: 0px;
+}
 
-    setTimeout(() => {
-        document.getElementById('toasted').removeChild(node)
-    }, timeout - 50)
+.top {
+  top: 0px;
+}
+
+.center {
+  left: 25%;
+  width: 50%;
+}
+
+.left{
+  left: 10px;
+  width: 225px;
+}
+
+.right{
+  right: 50px;
+  width: 225px;
+}
+
+#toasted{
+  display: flex;
+  text-align: center;
+  position: absolute;
+  z-index: 9999;
+  opacity: 1;
+  flex-direction: column;
+  list-style: none;
+}
+
+.toasted {
+  width: 100%;
+  padding: 15px;
+  color: white;
+  font-weight: bold;
+  border-radius: 5px;
+  -webkit-box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.75);
+  margin-bottom: 10px;
+}
+
+.success{
+  background-color: rgba(30, 156, 4, 0.84);
+}
+
+.info{
+  background-color: rgba(4, 102, 156, 0.84);
+}
+
+.warning{
+  background-color: rgba(238, 114, 0, 0.84);
+}
+
+.error{
+  background-color: rgba(158, 40, 40, 0.84);
+}`
+
+style.type = 'text/css'
+style.appendChild(document.createTextNode(css))
+
+
+//Toasted Function - You can change the default values for the toast here
+//classname can be info, warning, success and error OR you can create another style in the css above
+//position should respect the order VERTICAL first HORIZONTAL second. IE: top left or bottom right
+//timeout can be any number in milliseconds. 5000 is 5 seconds
+//if true, OnTop displays new toats above the last one
+const Toasted = (children, type = 'info', position = 'bottom center', timeout = 5000, onTop = false) => {
+
+  let duration = timeout/1000
+  let elementExists = document.getElementById('toasted')
+
+  let ul = document.createElement('ul')
+  ul.id = 'toasted'
+  ul.className = `${position} onTop--${onTop}`
+
+  let node = document.createElement('li')
+
+  const Tstr = () => (
+    <div
+      className={`toasted ${type}`}
+      style={{animation: `${position.split(' ')[0]} ${duration}s`}}
+    >
+      {children}
+    </div>
+  )
+
+  if(elementExists) {
+    elementExists.appendChild(node)
+    render(<Tstr /> , node)
+  } else {
+    document.getElementsByTagName('head')[0].appendChild(style)
+
+    document.body.appendChild(ul)
+    ul.appendChild(node)
+    render(<Tstr /> , node)
+  }
+
+  setTimeout(() => {
+    document.getElementById('toasted').removeChild(node)
+  }, timeout - 50)
 }
 
 export default Toasted
